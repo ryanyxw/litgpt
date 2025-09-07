bcb8c683ca965243d966adfc250f9a38dce47a63
ryanyxw <ryanywan@usc.edu>
2025-09-07 01:02:37 -0700

    update

--- litgpt/pretrain.py
+++ litgpt/pretrain.py
@@ -349,6 +349,8 @@ def fit(
 
         input_ids = train_data[:, 0 : model.max_seq_length].contiguous().long()
         targets = train_data[:, 1 : (model.max_seq_length + 1)].contiguous().long()
+        import pdb
+        pdb.set_trace()
 
         is_accumulating = state["iter_num"] % train.gradient_accumulation_iters(devices, num_nodes) != 0
         with fabric.no_backward_sync(model, enabled=is_accumulating):
