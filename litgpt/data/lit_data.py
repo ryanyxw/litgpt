ede66c253361e2df9d04b5d72c6ad12c4f2de238
ryanyxw <ryanywan@usc.edu>
2025-09-07 00:57:16 -0700

    update

--- litgpt/data/lit_data.py
+++ litgpt/data/lit_data.py
@@ -59,8 +59,6 @@ class LitData(DataModule):
             seed=self.seed,
         )
 
-        import pdb
-        pdb.set_trace()
         dataloader = StreamingDataLoader(
             dataset, batch_size=self.batch_size, pin_memory=True, num_workers=self.num_workers, drop_last=True
         )
